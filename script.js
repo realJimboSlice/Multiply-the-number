f93c3f2a378ba78fe42984d8c7341a38bd24556d
realJimboSlice <jimm7493@stud.kea.dk>
2024-03-13 14:19:46 +0100

    Finished assignment and readme formatting

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 function multiply(number) {
-  //your code here
+  // Number is multiplied by 5 to the power length of the number (using toString method to convert number to string and then getting the length of the string using length property of string object)
+  let newNum = number * 5 ** number.toString().length;
+  return newNum;
 }
